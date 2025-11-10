dfe6c513a4e5b02aeaa8b0de251da5416fe31c36
kyostip <kyosti@perakasari.com>
2025-11-10 08:32:49 +0200

    csvs assigment updates

---
+++ args.py
@@ -0,0 +1,8 @@
+# This file contains arguments and configurations
+
+#image directories
+xrays ="./images/xrays/"
+masks = ".images/masks/"
+
+# training parameters
+epochs = 10
---
+++ main.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+
+from model import *
+from args import epochs, xrays, masks
+
+
+if __name__ == "__main__":
+    print(f"hello, epochs are {epochs}")
+
